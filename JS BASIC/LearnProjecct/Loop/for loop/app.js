c74db83efa2f72140c30b0d3643a4ad7d54a89ba
PinkPigPinkPig <vuquytuan2706@gmail.com>
2022-05-03 13:33:31 +0700

    Practice Loop 3/5/2022

---
+++ JS BASIC/LearnProjecct/Loop/for loop/app.js
@@ -0,0 +1,6 @@
+let text = "";
+for(let i = 1; i <= 5; i++){
+    text += "The Number is " + i + "<br>";
+}
+
+document.getElementById("demo").innerHTML = text;
---
+++ JS BASIC/LearnProjecct/Loop/using while loop/Bài 1/app.js
@@ -0,0 +1,10 @@
+let num;
+
+let total = 0;
+
+while( num != -1 ) {
+    num = parseInt(prompt("Enter a number: "));
+    alert(num);
+    total += num;
+    alert("Tổng: " + total);
+ } 
